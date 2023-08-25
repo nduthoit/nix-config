843e9aa1d911b103083e982b42f6e3ee5699b9dc
Nathan Duthoit <nathan.duthoit@gmail.com>
2023-08-25 11:46:04 -0400

    config: adjust shell styling

--- home/fish.nix
+++ home/fish.nix
@@ -46,30 +46,30 @@ in
         set -xg LS_COLORS (${pkgs.vivid}/bin/vivid generate solarized-$term_background)
 
         # Set color variables
-        if test "$term_background" = light
-          set emphasized_text  brgreen  # base01
-          set normal_text      bryellow # base00
-          set secondary_text   brcyan   # base1
-          set background_light white    # base2
-          set background       brwhite  # base3
-        else
-          set emphasized_text  brcyan   # base1
-          set normal_text      brblue   # base0
-          set secondary_text   brgreen  # base01
-          set background_light black    # base02
-          set background       brblack  # base03
-        end
+        # if test "$term_background" = light
+        #   set emphasized_text  brgreen  # base01
+        #   set normal_text      bryellow # base00
+        #   set secondary_text   brcyan   # base1
+        #   set background_light white    # base2
+        #   set background       brwhite  # base3
+        # else
+        #   set emphasized_text  brcyan   # base1
+        #   set normal_text      brblue   # base0
+        #   set secondary_text   brgreen  # base01
+        #   set background_light black    # base02
+        #   set background       brblack  # base03
+        # end
 
         # Set Fish colors that change when background changes
-        set -g fish_color_command                    $emphasized_text --bold  # color of commands
-        set -g fish_color_param                      $normal_text             # color of regular command parameters
-        set -g fish_color_comment                    $secondary_text          # color of comments
-        set -g fish_color_autosuggestion             $secondary_text          # color of autosuggestions
-        set -g fish_pager_color_prefix               $emphasized_text --bold  # color of the pager prefix string
-        set -g fish_pager_color_description          $selection_text          # color of the completion description
-        set -g fish_pager_color_selected_prefix      $background
-        set -g fish_pager_color_selected_completion  $background
-        set -g fish_pager_color_selected_description $background
+        # set -g fish_color_command                    $emphasized_text --bold  # color of commands
+        # set -g fish_color_param                      $normal_text             # color of regular command parameters
+        # set -g fish_color_comment                    $secondary_text          # color of comments
+        # set -g fish_color_autosuggestion             $secondary_text          # color of autosuggestions
+        # set -g fish_pager_color_prefix               $emphasized_text --bold  # color of the pager prefix string
+        # set -g fish_pager_color_description          $selection_text          # color of the completion description
+        # set -g fish_pager_color_selected_prefix      $background
+        # set -g fish_pager_color_selected_completion  $background
+        # set -g fish_pager_color_selected_description $background
       '' + optionalString config.programs.bat.enable ''
 
         # Use correct theme for `bat`.
@@ -124,7 +124,7 @@ in
   # Configuration that should be above `loginShellInit` and `interactiveShellInit`.
   programs.fish.shellInit = ''
     set -U fish_term24bit 1
-    ${optionalString pkgs.stdenv.isDarwin "set-background-to-macOS"}
+    # ${optionalString pkgs.stdenv.isDarwin "set-background-to-macOS"}
   '';
 
   programs.fish.interactiveShellInit = ''
