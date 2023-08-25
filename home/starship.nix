bc9eda3af113d38bb2ca3b8baf46bbafa56543bf
Nathan Duthoit <nathan.duthoit@gmail.com>
2023-08-25 11:41:07 -0400

    config: enable starship fish integration

--- home/starship.nix
+++ home/starship.nix
@@ -3,10 +3,13 @@
   # https://rycee.gitlab.io/home-manager/options.html#opt-programs.starship.enable
   programs.starship.enable = true;
 
+  programs.starship.enableFishIntegration = true;
+
   programs.starship.settings = {
     # See docs here: https://starship.rs/config/
     # Symbols config configured ./starship-symbols.nix.
 
+    command_timeout = 600;
     directory.fish_style_pwd_dir_length = 1; # turn on fish directory truncation
     directory.truncation_length = 2; # number of directories not to truncate
     # hostname.style = "bold green"; # don't like the default
